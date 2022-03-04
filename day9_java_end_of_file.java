e5ff4cac4dd51f4be58c1168e29fbb922d82ec4a
NURAISYAH D0221040 C <100023823+NURAISYAHD0221040@users.noreply.github.com>
2022-03-04 09:19:00 +0700

    day 9

--- day9_java_end_of_file.java
+++ day9_java_end_of_file.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class day9_java_end_of_file {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
