7382e03be11171b6adc1da7007227826e8e9a5a6
NURAISYAH D0221040 C <100023823+NURAISYAHD0221040@users.noreply.github.com>
2022-03-04 09:06:10 +0700

    day 10

--- day10_Static_Initializer_Block.java
+++ day10_Static_Initializer_Block.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class day10_Static_Initializer_Block {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
